91f476d8cb047c219c845b91aa8a2ed116f80175
dcooney <dcooney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-12 02:39:11 +0000

    admin update
    git-svn-id: https://plugins.svn.wordpress.org/ajax-load-more/trunk@964257 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/admin.php
+++ admin/admin.php
@@ -488,7 +488,7 @@ function alm_shortcode_builder_page(){ ?>
 		   	<div class="cta">
 					<h3><?php _e('Did you know?', ALM_NAME); ?></h3>
 					<img src="<?php echo ALM_ADMIN_URL; ?>img/add-ons/shortcode-editor.jpg"><br/>
-					<?php _e('<p class="addon-intro">You can generate shortcodes while editing pages!</p><p>Look for the Ajax Load More [A] icon in the content editor toolbar and the <a href="?page=ajax-load-more-shortcode-builder">shortcode builder</a> will open.', ALM_NAME); ?></p>
+					<?php _e('<p class="addon-intro">You can generate shortcodes while editing pages!</p><p>Click the Ajax Load More icon in the content editor toolbar and the <a href="?page=ajax-load-more-shortcode-builder">shortcode builder</a> will open in an overlay window.', ALM_NAME); ?></p>
 		   	</div>
 	   </div>
 	</div>
