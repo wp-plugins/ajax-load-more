73b020c4433cbc89389560bc8c7c88c42ce8fcdd
dcooney <dcooney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-13 14:22:47 +0000

    2.8.1.2
    git-svn-id: https://plugins.svn.wordpress.org/ajax-load-more/tags/2.8.1.2@1264924 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/includes/cta/extend.php
@@ -0,0 +1,1 @@
+<div class="clear"></div><div class="call-out"><?php _e('Unlock additional templates with the <a href="?page=ajax-load-more-add-ons" target="_parent">Custom Repeaters add-on</a>', 'ajax-load-more'); ?> <a class="cnkt-button" href="https://connekthq.com/plugins/ajax-load-more/custom-repeaters" target="_blank"><?php _e('More Info', 'ajax-load-more'); ?></a></div>
